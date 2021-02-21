b451e00cb140f09dec54860ae4535891eb9e6b16
Mahmudul <mahmudul@example.com>
2021-02-22 00:50:45 +0600

    first commit

---
+++ user_auth/Login_App/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Login_App.models import UserInformation
+# Register your models here.
+
+
+admin.site.register(UserInformation)
---
+++ user_auth/Login_App/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path, include
+from . import views
+
+
+app_name = 'Login_App'
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+]
